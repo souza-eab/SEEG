6eeac86b7b8366deb846767f48e77607fb6338d2
souza-eab <79071899+souza-eab@users.noreply.github.com>
2021-08-11 17:06:34 -0300

    update

--- 2_General_map/2_0_filter_transitions.js
+++ 2_General_map/2_0_filter_transitions.js
@@ -88,6 +88,7 @@ var filterParams = [
    {classValue: 0, maxSize: 5}
 ];
 
+// Verificar se à partir daqui considera apenas 1990 ou de 1985
 //A partir daqui trabalhamos apenas a partir de 1990, que é o período inicial apresentado pelo Inventário Nacional
 var anos = ['1985','1986','1987','1988','1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020'];
 
