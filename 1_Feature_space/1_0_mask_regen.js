19028dafc72c0e8f3986ed03ac08686491ca140c
souza-eab <79071899+souza-eab@users.noreply.github.com>
2021-08-12 20:04:21 -0300

    Update 1_0_mask_regen.js

--- 1_Feature_space/1_0_mask_regen.js
+++ 1_Feature_space/1_0_mask_regen.js
@@ -2,6 +2,7 @@
 // For any issue/bug, please write to <edriano.souza@ipam.org.br>; <dhemerson.costa@ipam.org.br>; <barbara.zimbres@ipam.org.br>
 // Developed by: IPAM, SEEG and OC
 // Citing: SEEG/Observatório do Clima and IPAM
+// Processing time <1h> in Google Earth Engine
 
 // Set assets
 // Asset Biomes Brazil
