da3d70014422810dc1b61c0bde97e7d5a2f37469
souza-eab <79071899+souza-eab@users.noreply.github.com>
2021-08-13 13:48:33 -0300

    Update 1_0_mask_regen.js

--- 1_Feature_space/1_0_mask_regen.js
+++ 1_Feature_space/1_0_mask_regen.js
@@ -18,6 +18,8 @@ var colecao6 = ee.ImageCollection("projects/mapbiomas-workspace/COLECAO6/mapbiom
 var col6floresta85 = colecao6.select('classification_1985').remap(
                   [3, 4, 5, 6, 11, 12, 13, 9, 15,  19, 20, 21,  23, 24, 25,  27, 29, 30, 31, 32, 33, 36, 39, 40, 41, 42, 43, 44, 45, 49],
                   [1, 1, 1, 1,  1,  1,  1, 0,  0,   0,  0,  0,   9,  0,  0,   9,  9,  0,  0,  9,  9,  0,  0,  0,  0,  0,  0,  0,  0, 1]);
+
+// Include 6/49 
                
 // Changing names of bands 
 col6floresta85 = col6floresta85.select([0],['regen1985']).int8();
