70b02997cc5f30d0601c63a343267274f9fc17cd
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2021-09-14 12:31:47 -0300

    Update 3_0_rectCT_PAM_v1.js

--- 3_QCN/3_0_rectCT_PAM_v1.js
+++ 3_QCN/3_0_rectCT_PAM_v1.js
@@ -11,11 +11,12 @@
 // @. ~~~~~~~~~~~~~~ // 
   
   /* @. Set user parameters */// eg.
-var dir_output = 'projects/mapbiomas-workspace/SEEG/2021/QCN_stp2/';
+var dir_output = 'projects/mapbiomas-workspace/SEEG/2021/QCN_stp2_v1/';
 var version = '1';
 
 // Define classes to be assesed as 'reference class' into QCN
-var list_classes = [1];
+var list_classes = [3, 4, 12];
+
 // Define years of Mapbiomas to be compared with QCN reference class
 var list_mapb_years = [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997,
                        1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
@@ -23,7 +24,7 @@ var list_mapb_years = [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 199
 
 // Define reclassification matrix
 var raw_mapbiomas = [3, 4, 5, 9, 10, 11, 12, 13, 14, 15, 18, 19, 20, 21, 22, 23, 24, 25, 26, 29, 30, 31, 32, 33, 36, 39, 41];   // Palets add other new classes 
-var design5 =       [3, 4, 0, 0,  0, 0,  12,  0, 0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0];     // Ungroup {Forest) + Grass  
+var design5 =       [3, 4, 0, 0,  0, 0,  12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0];     // Ungroup {Forest) + Grass  
 
 ///////////////////////////////////////
   /* @. Don't change below this line *///
@@ -43,14 +44,31 @@ var pala = pal.kovesi.rainbow_bgyr_35_85_c72[7];
 
 // total stock
 var pam_tot = ee.Image('users/edrianosouza/QCN/pam_ctotal4inv');
-var soc = ee.Image('users/edrianosouza/soil_co2/BR_SOCstock_0-30_t_ha');
+//var soc = ee.Image('users/edrianosouza/soil_co2/BR_SOCstock_0-30_t_ha');
 
 // brazilian states
 var states = ee.Image('projects/mapbiomas-workspace/AUXILIAR/estados-2016-raster');
 Map.addLayer(states.randomVisualizer(), {}, 'states', false);
 
+
 // Import LCLUC data
-var qcn = ee.Image("projects/mapbiomas-workspace/SEEG/2021/QCN_stp1/pam_12");
+var qcnF = ee.Image("projects/mapbiomas-workspace/SEEG/2021/QCN_stp1/pam_3");
+var qcnS = ee.Image("projects/mapbiomas-workspace/SEEG/2021/QCN_stp1/pam_4");
+var qcnC = ee.Image("projects/mapbiomas-workspace/SEEG/2021/QCN_stp1/pam_12");
+
+// reclassificiar
+var qcnF = qcnF.remap([0, 1], [0, 3]);
+var qcnS = qcnS.remap([0, 1], [0, 4]);
+var qcnC = qcnC.remap([0, 1], [0, 12]);
+
+// fazer o blend só com as classes - descartar quando value == 0
+var qcn = qcnF.updateMask(qcnF.eq(3)).blend(qcnS.updateMask(qcnS.eq(4)).blend(qcnC.updateMask(qcnC.eq(12))));
+
+var pal = require('users/gena/packages:palettes');
+var palt = pal.matplotlib.viridis[7];
+
+Map.addLayer(qcn, {min: 0, max: 12, palette: palt}, 'QCN_Reclass_QGIS');
+
 var colecao5 = ee.ImageCollection("projects/mapbiomas-workspace/COLECAO5/mapbiomas-collection50-integration-v8").mosaic();
 
 // Plot inspection
@@ -63,14 +81,16 @@ Map.addLayer(pam_tot, {min: 0, max: 168, palette: palt}, 'CT 1985');
 // create empty recipes
 var image_static = ee.Image([]);
 var image_accumm = ee.Image([]);
+var temp = ee.Image([]);
+var temp2 = ee.Image([]);
 
+// For each year of MapBiomas
+ list_mapb_years.forEach(function(year_j){
 // For each QCN reference class [i]
-list_classes.forEach(function(class_i) {
+  list_classes.forEach(function(class_i) {
   // Mask QCN only to reference class
   var qcn_i = qcn.updateMask(qcn.eq(class_i));
   
-  // For each year of MapBiomas
-  list_mapb_years.forEach(function(year_j){
     // Mask MapBiomas by QCN
     var mapb_qcn_ij = colecao5.select(['classification_' + year_j]).updateMask(qcn_i.eq(class_i));
     // Perform reclassification according definied matrix
@@ -81,7 +101,7 @@ list_classes.forEach(function(class_i) {
         pam_tot_rect = pam_tot_rect.rename('rect_' + year_j);
     
     // perform QCN correction by brazilian state - static //
-    var pam_tot_rect = pam_tot.where(states.eq(43).and(mapb_qcn_ij_d5.eq(12)), 4.560158311); // RN
+    var pam_tot_rect = pam_tot_rect.where(states.eq(43).and(mapb_qcn_ij_d5.eq(12)), 4.560158311); // RN
         pam_tot_rect = pam_tot_rect.rename('rect_' + year_j);
         
     // perform QCN correction by brazilian state - cumulative - considers the rect of the last year //
@@ -93,7 +113,7 @@ list_classes.forEach(function(class_i) {
     // perform QCN correction by brazilian state - cumulative - considers the rect of the last year //
     // first year dont have previous year
     if (year_j == 1985) {
-      var pam_tot_rect2 = pam_tot.where(states.eq(43).and(mapb_qcn_ij_d5.eq(12)), 4.560158311); // RN
+      var pam_tot_rect2 = pam_tot_rect2.where(states.eq(43).and(mapb_qcn_ij_d5.eq(12)), 4.560158311); // RN
           pam_tot_rect2 = pam_tot_rect2.rename('rect_' + year_j);
     }
    
@@ -105,17 +125,36 @@ list_classes.forEach(function(class_i) {
     }
     // if year is greater than 1985, considers the previous year
     if (year_j > 1985) {
-      var r_last_year = image_accumm.select(['rect_' + (year_j -1)]);
-      var pam_tot_rect2 = r_last_year.where(states.eq(43).and(mapb_qcn_ij_d5.eq(12)), 4.560158311);   // RN
+      var pam_tot_rect2 = pam_tot_rect2.where(states.eq(43).and(mapb_qcn_ij_d5.eq(12)), 4.560158311);   // RN
           pam_tot_rect2 = pam_tot_rect2.rename('rect_' + year_j);
     }
+
+        
+        // mix corrections
+        // static
+        if (class_i == 3) {
+          temp = pam_tot_rect;
+        }
+        if (class_i == 12) {
+          temp = temp.blend(pam_tot_rect.updateMask(qcn_i.eq(class_i)));
+          // paste as band
+          image_static = image_static.addBands(temp);
+        }
+        
+        // accumulated
+        if (class_i == 3) {
+          temp2 = pam_tot_rect2;
+        }
+        if (class_i == 12) {
+          temp2 = temp2.blend(pam_tot_rect2.updateMask(qcn_i.eq(class_i)));
+          // paste as band
+          image_accumm = image_accumm.addBands(pam_tot_rect2);
+        }
     
-    // add results as a band 
-    image_static = image_static.addBands(pam_tot_rect);
-    image_accumm = image_accumm.addBands(pam_tot_rect2);
-    
+    });
+
   });
-});
+    
 
 print('static', image_static);
 print('accumulated', image_accumm);
@@ -127,8 +166,8 @@ Map.addLayer(image_accumm.select(['rect_2019']),  {min: 0, max: 168, palette: pa
 // export as GEE asset
 Export.image.toAsset({
     "image": image_static.toFloat(),
-    "description": 'pam_pclass_static_12_all',
-    "assetId": dir_output + 'pam_pclas_static_12_all',
+    "description": 'pam_pclass_static_all',
+    "assetId": dir_output + 'pam_pclas_static_all',
     "scale": 30,
     "pyramidingPolicy": {
         '.default': 'mode'
