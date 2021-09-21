c7486534576463455e04fc42edfefa12ef01db1f
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2021-09-21 09:45:09 -0300

    Delete 98_upload.sh

--- 3_QCN/tilerize/unused/98_upload.sh
+++
@@ -1,8 +0,0 @@
-## set init environment using anaconda
-pip install geeup
-geeup init
-earthengine authenticate
-
-## upload
-cd C:\Users\dhemerson.costa\OneDrive - INSTITUTO DE PESQUISA AMBIENTAL DA AMAZÔNIA\IPAM\7_SEEG\1_QCN\r_code
-geeup upload -u dh.conciani@gmail.com --source C:\Users\dhemerson.costa\Desktop\to_up --dest projects/mapbiomas-workspace/SEEG/2021/QCN/QCN_30m --metadata C:\Users\dhemerson.costa\Desktop\metadata.csv
