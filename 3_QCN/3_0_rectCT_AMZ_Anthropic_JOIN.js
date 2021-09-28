d6a84b336e73173f72daf1f1009ff2c11fe2be78
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2021-09-28 18:03:15 -0300

    Update 3_0_rectCT_AMZ_Anthropic_JOIN.js

--- 3_QCN/3_0_rectCT_AMZ_Anthropic_JOIN.js
+++ 3_QCN/3_0_rectCT_AMZ_Anthropic_JOIN.js
@@ -3,7 +3,7 @@ var assets = ee.List.sequence(6,6,1).getInfo();
 var address =   'projects/mapbiomas-workspace/SEEG/2021/QCN/tile_id_';
 
 var tiles = [
-  // 1,11,14,15,16,18,19,2,20,21,22,23,24,27,28,3,32,4,6,7,8,9 // desordenado
+  // 1,11,12, 14,15,16,17,18,19,2,20,21,22,23,24,27,28,3,32,4,6,7,8,9 // desordenado
   1,2,3,4,6,7,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,27,28,32 // Total de tiles 25 // Exclusion
 
   ];
