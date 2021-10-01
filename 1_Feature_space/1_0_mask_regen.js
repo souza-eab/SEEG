388271ed24192109c8fcf692d9b319af6c3a367f
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2021-10-01 16:00:20 -0300

    Update Class Mapbiomas 6.0
    As classes da primeira versão rodada, seguia a classe 36 que foi separada em 46,47 e 48, ajustes nas classes do Mapbiomas;

--- 1_Feature_space/1_0_mask_regen.js
+++ 1_Feature_space/1_0_mask_regen.js
@@ -1,30 +1,33 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////// SCRIPT TO GENERATE REGENERATION MASKS FROM A COLLECTION OF MAPBIOMAS (eg. col 6.0) /////////////////////////////////////////////////////////
 ////////////////////////////////// For any issue/bug, please write to <edriano.souza@ipam.org.br>; <dhemerson.costa@ipam.org.br>; <barbara.zimbres@ipam.org.br> ///////////////
-////////////////////////////////// Coordination: Julia Shimbo and Ane Alencar /////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////// Coordination: Barbara Zimbres, Julia Shimbo, and Ane Alencar ///////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////// Developed by: IPAM, SEEG and OC ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////// Citing: SEEG/Observatório do Clima and IPAM ////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////// Processing time <2h> in Google Earth Engine ////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// @. UPDATE HISTORIC //
+// 1: SCRIPT TO GENERATE REGENERATION MASKS FROM A COLLECTION OF MAPBIOMAS (eg. col 6.0)
+// 1.1: Acess Asset MapBiomas and Biomes BRAZIL
+// 1.1: Remap layer col. 6.0 Mapiomas 
+// @. ~~~~~~~~~~~~~~ // 
+
+/* @. Set user parameters */// eg.
+var dir_output = 'projects/mapbiomas-workspace/SEEG/2021/Col9/';
+
 // Set assets
 // Asset Biomes Brazil
 var Bioma = ee.FeatureCollection("users/SEEGMapBiomas/bioma_1milhao_uf2015_250mil_IBGE_geo_v4_revisao_pampa_lagoas"); 
 
-//// brazilian biomes?
-var biomes = ee.Image('projects/mapbiomas-workspace/AUXILIAR/biomas-2019-raster');
-
 // Add ImageCollection Mapbiomas 6.0
 var colecao6 = ee.ImageCollection("projects/mapbiomas-workspace/COLECAO6/mapbiomas-collection60-integration-v0-12").mosaic();
 
-// Retire class 13; 27; 
 //Remap layers for native vegetation in 1985 to 1; what is anthropic, is 0; and what does not apply, is 9
 var col6floresta85 = colecao6.select('classification_1985').remap(
-                  [3, 4, 5, 11, 12,  9, 15,  19, 20, 21,  23, 24, 25,  29, 30, 31, 32, 33, 36, 39, 40, 41,  46, 47, 48],
-                  [1, 1, 1,  1,  1,  0,  0,   0,  0,  0,   9,  0,  0,   9,  0,  0,  9,  9,  0,  0,  0,  0,   0,  0,  0]);
+                  [3,4,5,9,11,12,13,15,20,21,23,24,25,29,30,31,32,33,39,40,41,46,47,48,49],
+                  [1,1,1,0, 1, 1, 1, 0, 0, 0, 9, 0, 0, 9, 0, 0, 9, 9, 0, 0, 0, 0, 0, 0, 1]);
 
-// Include New Class 49 in 2019 _ {1.4. Wooded Restinga}
-               
 // Changing names of bands 
 col6floresta85 = col6floresta85.select([0],['regen1985']).int8();
 
@@ -32,12 +35,12 @@ col6floresta85 = col6floresta85.select([0],['regen1985']).int8();
 var anos = ['1985','1986','1987','1988','1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020'];
 
 // Complete doing the same thing for the other years
-for (var i_ano=0;i_ano<anos.length; i_ano++){ /////nao tá em roxo length
+for (var i_ano=0;i_ano<anos.length; i_ano++){ 
   var ano = anos[i_ano];
 
   var col6flor = colecao6.select('classification_'+ano).remap(
-                  [3, 4, 5, 6, 11, 12, 13, 9, 15,  19, 20, 21,  23, 24, 25,  27, 29, 30, 31, 32, 33, 36, 39, 40, 41, 42, 43, 44, 45, 49],
-                  [1, 1, 1, 1,  1,  1,  1, 0,  0,   0,  0,  0,   9,  0,  0,   9,  9,  0,  0,  9,  9,  0,  0,  0,  0,  0,  0,  0,  0, 1]);
+                  [3,4,5,9,11,12,13,15,20,21,23,24,25,29,30,31,32,33,39,40,41,46,47,48,49],
+                  [1,1,1,0, 1, 1, 1, 0, 0, 0, 9, 0, 0, 9, 0, 0, 9, 9, 0, 0, 0, 0, 0, 0, 1]);
                     
   col6floresta85 = col6floresta85.addBands(col6flor.select([0],['regen'+ano])).int8();
 }
@@ -139,13 +142,13 @@ for (var i = 1989; i < 2019; i++) {
 regen = regen.addBands(mask19).addBands(mask20);
 print(regen);
 
-Map.addLayer(regen.select('regen1999'), {'min': 0,'max': 1, 'palette': 'blue'},'Regen_1999');
+Map.addLayer(regen.select('regen2019'), {'min': 0,'max': 1, 'palette': 'blue'},'Regen_2019');
 Map.addLayer(regen.select('regen2020'), {'min': 0,'max': 1, 'palette': 'blue'},'Regen_2020');
 
 Export.image.toAsset({
     "image": regen.unmask(0).uint8(),
     "description": 'regenSEEGc6',
-    "assetId": 'projects/mapbiomas-workspace/SEEG/2021/regenSEEGc6', //inserir aqui o endereço e o nome do Asset a ser exportado
+    "assetId": dir_output + 'regenSEEGc6', //inserir aqui o endereço e o nome do Asset a ser exportado
     "scale": 30,
     "pyramidingPolicy": {
         '.default': 'mode'
